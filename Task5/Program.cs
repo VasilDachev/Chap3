b701d44ae1662c34f9af350f5350c275a540beb5
VasilDachev <vasildachevnew@gmail.com>
2018-11-01 12:11:28 +0200

    Add project files.

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете израз, който изчислява площта на трапец по дадени страни a и b и височина h.
+
+
+        }
+    }
+}
