4ed381c952f85f0a46f3d056811fee9dd6e6548b
VasilDachev <vasildachevnew@gmail.com>
2018-11-03 11:10:17 +0200

    Update

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -11,7 +11,18 @@ namespace Task5
         static void Main(string[] args)
         {
             //Напишете израз, който изчислява площта на трапец по дадени страни a и b и височина h.
+            double h, a, b;
+            Console.WriteLine("Énter h:");
+            h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = double.Parse(Console.ReadLine());
 
+            double S;
+            S = ((a + b) * h) / 2;
+            Console.WriteLine("S = {0}",S);
+            Console.ReadLine();
 
         }
     }
